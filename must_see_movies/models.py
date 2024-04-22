2a6aac120723b19d927bf87b581266c35888a0da
Ben Purinton <ben.purinton@gmail.com>
2024-04-22 20:12:26 +0000

    add directory model and foreign key in movie

--- must_see_movies/models.py
+++ must_see_movies/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.urls import reverse
 
+class Director(models.Model):
+    bio = models.TextField(blank=True, null=True)
+    dob = models.DateField(blank=True, null=True)
+    image = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("director", kwargs={"pk": self.pk})
+
 class Movie(models.Model):
     description = models.TextField()
     duration = models.IntegerField()
@@ -9,6 +23,7 @@ class Movie(models.Model):
     year = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    director = models.ForeignKey(Director, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
