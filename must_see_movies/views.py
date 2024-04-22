da14a9cbb4e7c6812f923fc356b029ca7eba47a7
Ben Purinton <ben.purinton@gmail.com>
2024-04-22 17:42:30 +0000

    add movie CRUD views

--- must_see_movies/views.py
+++ must_see_movies/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from .models import Movie
 
-# Create your views here.
+class MovieListView(ListView):
+    model = Movie
+    template_name = "movie_list.html"
+
+class MovieDetailView(DetailView):
+    model = Movie
+    template_name = "movie_detail.html"
+
+class MovieCreateView(CreateView):
+    model = Movie
+    template_name = "movie_new.html"
+    fields = ("description", "duration", "image", "title", "year")
+
+class MovieUpdateView(UpdateView):
+    model = Movie
+    template_name = "movie_edit.html"
+    fields = ("description", "duration", "image", "title", "year")
+
+class MovieDeleteView(DeleteView):
+    model = Movie
+    template_name = "movie_delete.html"
+    success_url = "/movies/"
