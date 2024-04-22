e17467898b366033e7781603c48c8188f489e77a
Ben Purinton <ben.purinton@gmail.com>
2024-04-22 19:16:39 +0000

    update admin display for Movie

--- must_see_movies/admin.py
+++ must_see_movies/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Movie
 
-admin.site.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = [
+        "pk",
+        "title",
+        "year",
+        "created_at",
+        "updated_at",
+    ]   
+
+admin.site.register(Movie, MovieAdmin)
