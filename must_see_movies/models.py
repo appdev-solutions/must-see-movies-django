a6720b9d68b1fb66d7e15c862a769c5c963ebc7b
Ben Purinton <ben.purinton@gmail.com>
2024-04-22 21:41:17 +0000

    add Actor model and Character joining model

--- must_see_movies/models.py
+++ must_see_movies/models.py
@@ -30,3 +30,24 @@ class Movie(models.Model):
 
     def get_absolute_url(self):
         return reverse("movie", kwargs={"pk": self.pk})
+
+class Actor(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("actor", kwargs={"pk": self.pk})
+
+class Character(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
