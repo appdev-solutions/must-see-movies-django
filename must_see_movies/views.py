d340854c96deaced8e37c4bf92139f3e75928d9e
Ben Purinton <ben.purinton@gmail.com>
2024-04-22 20:24:20 +0000

    add director CRUD views, add director to movie CRUD

--- must_see_movies/views.py
+++ must_see_movies/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from .models import Movie
+from .models import Movie, Director
 
 class MovieListView(ListView):
     model = Movie
@@ -14,14 +14,37 @@ class MovieDetailView(DetailView):
 class MovieCreateView(CreateView):
     model = Movie
     template_name = "movie_new.html"
-    fields = ["description", "duration", "image", "title", "year"]
+    fields = ["description", "duration", "image", "title", "year", "director"]
 
 class MovieUpdateView(UpdateView):
     model = Movie
     template_name = "movie_edit.html"
-    fields = ["description", "duration", "image", "title", "year"]
+    fields = ["description", "duration", "image", "title", "year", "director"]
 
 class MovieDeleteView(DeleteView):
     model = Movie
     template_name = "movie_delete.html"
     success_url = reverse_lazy("movies")
+
+class DirectorListView(ListView):
+    model = Director
+    template_name = "director_list.html"
+
+class DirectorDetailView(DetailView):
+    model = Director
+    template_name = "director_detail.html"
+
+class DirectorCreateView(CreateView):
+    model = Director
+    template_name = "director_new.html"
+    fields = ["name", "bio", "dob", "image"]
+
+class DirectorUpdateView(UpdateView):
+    model = Director
+    template_name = "director_edit.html"
+    fields = ["name", "bio", "dob", "image"]
+
+class DirectorDeleteView(DeleteView):
+    model = Director
+    template_name = "director_delete.html"
+    success_url = reverse_lazy("directors")
