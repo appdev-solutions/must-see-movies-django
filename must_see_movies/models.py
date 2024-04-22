3485018eaca8b63055658851b471d216cf9944ce
Ben Purinton <ben.purinton@gmail.com>
2024-04-22 17:26:57 +0000

    add movie model

--- must_see_movies/models.py
+++ must_see_movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    description = models.TextField()
+    duration = models.IntegerField()
+    image = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
