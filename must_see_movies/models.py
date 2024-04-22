c85ae14765c55933fe3ebc35d4ec6609981f6dcb
Ben Purinton <ben.purinton@gmail.com>
2024-04-22 18:39:48 +0000

    get_absolute_url in Movie model

--- must_see_movies/models.py
+++ must_see_movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Movie(models.Model):
     description = models.TextField()
@@ -9,3 +10,6 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("movie", kwargs={"pk": self.pk})
